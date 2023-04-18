0efa2850b311fa25ca0c7864a54731919b2f145a
BrunnoRafa <brunor_17@hotmail.com>
2023-04-17 22:14:17 -0300

    Criando o componente de upload e ajustando o PhotoComponent para identificar quando deve buscar a url local ou não

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -2,24 +2,35 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Photo } from "./photo";
+import { map } from 'rxjs/operators';
 
 const API = 'http://localhost:3000';
+const STATUS_CODE_SUCESS = 200;
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-    listFromUser(userName: string) {
-        return this.http
-            .get<Photo[]>(API + '/' + userName + '/photos');       
-    }
+  listFromUser(userName: string) {
+    return this.http
+      .get<Photo[]>(API + '/' + userName + '/photos');
+  }
 
-    listFromUserPaginated(userName: string, page: number) {
-        const params = new HttpParams()
-            .append('page', page.toString());
+  listFromUserPaginated(userName: string, page: number) {
+    const params = new HttpParams()
+      .append('page', page.toString());
 
-        return this.http
-            .get<Photo[]>(API + '/' + userName + '/photos', { params });       
-    }    
+    return this.http
+      .get<Photo[]>(API + '/' + userName + '/photos', { params });
+  }
+
+  upload(description: string, allowComments: boolean, file: File) {
+    const formData = new FormData();
+    formData.append('description', description);
+    formData.append('allowComments', allowComments ? 'true' : 'false');
+    formData.append('imageFile', file);
+
+    return this.http.post(API + '/photos/upload', formData, { observe: 'response' });
+  }
 }
